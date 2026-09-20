1aeadfd73bd60cdb0ee5f9984c01dbe7caf93570
soyuzalpha <saepudinasep86@gmail.com>
2026-09-20 22:10:23 +0700

    chore: remove unused WebGL components, terminal subsystem, and deps

--- src/helpers/useIsomorphicEffect.js
+++
@@ -1,6 +0,0 @@
-"use client";
-"use client";
-import { useEffect, useLayoutEffect } from 'react';
-
-export const useIsomorphicLayoutEffect =
-  typeof window !== 'undefined' ? useLayoutEffect : useEffect;
